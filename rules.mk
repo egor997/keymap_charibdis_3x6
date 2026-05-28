6a36bc80daf53014367b0d8ac784a6692e8aaf48
egor997 <egorgavrileyko@gmail.com>
2026-05-28 20:34:59 +0200

    here is my keymap and rules file for charibdis mini keyboard

---
+++ rules.mk
@@ -0,0 +1,13 @@
+MCU = RP2040
+BOOTLOADER = rp2040
+CONSOLE_ENABLE = no
+SPLIT_KEYBOARD = yes
+
+# Включить скроллинг
+POINTING_DEVICE_ENABLE = yes
+SCROLLING_ENABLE = yes
+
+# Опционально: настройки скроллинга
+SCROLL_THRESH_RANGE = 0  # Чувствительность
+SCROLL_DIVISOR_V = 8     # Делитель для вертикального скроллинга
+SCROLL_DIVISOR_H = 8     # Делитель для горизонтального скроллинга
